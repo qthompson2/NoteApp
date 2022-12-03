218cec1ce5e0272ef13f21455556832fa9019066
q.thompson <q.thompson088@gmail.com>
2022-12-02 17:12:05 -0800

    Adds save button functionality

--- JS/script.js
+++ JS/script.js
@@ -3,6 +3,7 @@ const aside = document.querySelector('aside');
 const textArea = document.querySelector('textarea');
 const noteButtons = document.querySelector('.buttonsLow');
 const shadyButton = document.querySelector('#darkmode');
+const noteList = document.querySelector('aside > ul');
 //const wellLitButton = document.querySelector('body.darkMode main .buttonsUp button#darkmode');
 
 const darkmodeElements = [body, aside];
@@ -21,7 +22,7 @@ function darkMode(elements) {
     if (shadyButton.textContent == 'Dark Mode') {
         shadyButton.textContent = 'Light Mode';
     } else if (shadyButton.textContent == 'Light Mode'){
-        shadyButton.textContent = 'Dark Mode'
+        shadyButton.textContent = 'Dark Mode';
     }
 }
 
@@ -35,4 +36,12 @@ function newNote(elements) {
     for (let i = 0; i < elements.length; i += 1) {
         elements[i].classList.remove('hidden');
     }
+}
+
+function saveNote() {
+    let title = prompt('Enter note title.');
+    notesArray.push({'title':title, 'content':textArea.value});
+    let newListItem = document.createElement('li');
+    newListItem.textContent = title;
+    noteList.append(newListItem);
 }
