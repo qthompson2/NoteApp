cc4e649e8ad7ea74b92496a8b6252800474bdc01
q.thompson <q.thompson088@gmail.com>
2022-12-02 16:32:28 -0800

    Adds cancel button functionality

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,12 @@
 const body = document.querySelector('body');
 const aside = document.querySelector('aside');
+const textArea = document.querySelector('textarea');
+const noteButtons = document.querySelector('.buttonsLow');
 
-const elements = [body, aside]
+const darkmodeElements = [body, aside];
+const noteElements = [textArea, noteButtons];
+
+let notesArray = [];
 
 function toggleDarkMode(element) {
     element.classList.toggle('darkMode');
@@ -9,6 +14,12 @@ function toggleDarkMode(element) {
 
 function darkMode(elements) {
     for (let i = 0; i < elements.length; i += 1) {
-        toggleDarkMode(elements[i])
+        toggleDarkMode(elements[i]);
+    }
+}
+
+function cancelNote(elements) {
+    for (let i = 0; i < elements.length; i += 1) {
+        elements[i].classList.add('hidden');
     }
 }
